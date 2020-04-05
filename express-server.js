93f801b5c41bb55b20e3f8a6de0d761dd50f83b5
Leos Literak <leos@literak.cz>
2020-04-05 10:11:29 +0200

    added slow method

--- express-server.js
+++ express-server.js
@@ -4,6 +4,25 @@ const cors = require('cors');
 const app = express();
 app.options('/v1/users/:userId', cors(), () => {});
 
+app.get('/slow', cors(), async (req, res) => {
+    console.log("slow handler starts");
+    function myFunc(arg) {
+        console.log(`arg was => ${arg}`);
+    }
+    setTimeout(myFunc, 1500, 'funky');
+    console.log("slow resumed");
+
+    const response = {
+        api: '1.0',
+        status: 'OK'
+    };
+    res.status(200);
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(response);
+    return res;
+});
+
 app.get('/v1/users/:userId', cors(), async (req, res) => {
     console.log("getUser handler starts");
     const response = {
