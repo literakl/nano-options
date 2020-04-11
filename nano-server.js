545f749d9e71581364abb39badb2d2238a323308
Leos Literak <leos@literak.cz>
2020-04-11 10:34:38 +0200

    fixed probably naming clash

--- nano-server.js
+++ nano-server.js
@@ -7,54 +7,54 @@ app.options('/v1/users/:userId', cors());
 
 app.get('/slow', cors(), async (req, res) => {
     console.log("slow handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     setTimeout(() => res.send(response), 2000);
     return res;
 });
 
 app.get('/v1/users/:userId', cors(), async (req, res) => {
     console.log("getUser handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 });
 
 app.get('/v1/polls/', async (req, res) => {
     console.log("getPolls handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 });
 
 app.get('/bff/polls/last', auth.optional, async (req, res) => {
     console.log("getLastPoll handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 });
 
 app.get('/bff/polls/:slug', auth.optional, async (req, res) => {
     console.log("getPoll handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 })
 
 app.get('/bff/polls/:pollId/votes', async (req, res) => {
     console.log("getVotes handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 });
 
 app.get('/bff/polls/:pollId/votes', async (req, res) => {
     console.log("getVotes handler starts");
-    const response = prepareResponse(res);
+    const response = createDefaultResponse(res);
     res.send(response);
     return res;
 })
 
-function prepareResponse(res) {
+function createDefaultResponse(res) {
     const response = {
         api: '1.0',
         status: 'OK'
